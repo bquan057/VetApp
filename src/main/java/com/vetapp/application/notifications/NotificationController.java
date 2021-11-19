0e7dce0f6e89b068323e372a33f03b8bc12d3903
Brandon Quan <30513301+bquan057@users.noreply.github.com>
2021-11-18 22:03:35 -0700

    VET057-61. Implemented deleteNotification() endpoint.

--- src/main/java/com/vetapp/application/notifications/NotificationController.java
+++ src/main/java/com/vetapp/application/notifications/NotificationController.java
@@ -1,6 +1,7 @@
 package com.vetapp.application.notifications;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,16 +14,29 @@ public class NotificationController {
 	@Autowired
 	NotificationRepository notificationRepository;
 	
-	@GetMapping("/api/animals/{userid}/notifications")
+	@GetMapping("/api/users/{userid}/notifications")
 	public ResponseEntity<List<Notification>> getAnimalNotifications(@PathVariable String userid) {
 		return new ResponseEntity<>(notificationRepository.findByUseridContaining(userid), HttpStatus.ACCEPTED);
 	}
 	
-	@PostMapping("/api/animals/{userid}/notifications")
+	@PostMapping("/api/users/{userid}/notifications")
 	public ResponseEntity<Notification> addAnimalNotification(@PathVariable String userid, @RequestBody Notification newNotification) {
 		newNotification.setUserid(userid);
 		Notification notification = notificationRepository.save(newNotification);
 		return new ResponseEntity<>(notification, HttpStatus.ACCEPTED);
 	}
 	
+	@DeleteMapping("api/users/notifications/{notificationid}")
+    public ResponseEntity<String> deleteNotification(@PathVariable int notificationid){
+
+		try{
+            Optional<Notification> notification = notificationRepository.findById(notificationid);
+            if(notification.isPresent()){
+            	notificationRepository.delete(notification.get());
+            }
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }catch(Exception e){
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }
+    }
 }
