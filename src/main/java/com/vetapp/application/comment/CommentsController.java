f0d6874ed2e9e4ac83a627ef5b7c63f34aa016ab
Rohinesh Ram <rohinesh@ualberta.ca>
2021-11-19 12:40:18 -0700

    VET057-60: Fixed errors in comment controller

--- src/main/java/com/vetapp/application/comment/CommentsController.java
+++ src/main/java/com/vetapp/application/comment/CommentsController.java
@@ -1,26 +1,15 @@
 package com.vetapp.application.comment;
 
-import org.apache.coyote.Response;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.rmi.ServerException;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 
 @RestController
 public class CommentsController {
-    @Autowired
-    CommentRepository repository;
+    private final CommentRepository repository;
 
     CommentsController(CommentRepository repository){
         this.repository = repository;
@@ -28,22 +17,21 @@ public class CommentsController {
 
     @GetMapping("/api/animals/{animalid}/comment")
     public ResponseEntity<List<Comment>> all(@PathVariable String animalid) {
-        return new ResponseEntity<>(repository.findByAnimalidContaining(animalid), HttpStatus.OK);
+        return new ResponseEntity<>(repository.findByAnimalidContaining(animalid), HttpStatus.ACCEPTED);
     }
 
 
-    @PostMapping(path = "/api/animals/comment/{commentid}", consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Comment> create(@RequestBody Comment newComment) {
+    @PostMapping("/api/animals/{animalid}/comment")
+    public ResponseEntity<Comment> create(@PathVariable String animalid, @RequestBody Comment newComment) {
+        newComment.setAnimalid(animalid);
         Comment comment = repository.save(newComment);
         return new ResponseEntity<>(comment, HttpStatus.CREATED);
     }
 
     @PutMapping("/api/animals/comment/{commentid}")
     public ResponseEntity<Comment> updateComment(@PathVariable(value = "commentid") Long commentid,
-      @RequestBody Comment comment){
+                                                 @RequestBody Comment comment){
         Comment comments = repository.findById(commentid).orElseThrow();
-//        comments.setAnimalid(comment.getAnimalid());
         comments.setComment(comment.getComment());
         final Comment updatedComment = repository.save(comments);
         return ResponseEntity.ok(updatedComment);
