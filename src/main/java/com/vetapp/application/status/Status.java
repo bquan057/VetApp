af3825e94a30e1e44590df9ba97781cb49c1cf86
Brandon Quan <30513301+bquan057@users.noreply.github.com>
2021-11-18 19:17:27 -0700

    VET057-24, 26. Implemented getAnimalStatus() endpoint.

--- src/main/java/com/vetapp/application/status/Status.java
+++ src/main/java/com/vetapp/application/status/Status.java
@@ -1,5 +1,31 @@
 package com.vetapp.application.status;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="Status")
 public class Status {
 
+	public Status() {
+		this.statusid = 0;
+		this.animalid = "";
+		this.date = "";
+		this.description = "";
+		this.location = "";
+		this.status = "";
+	}
+	
+	@Id @GeneratedValue @Getter @Setter int statusid;
+	@Getter @Setter String animalid;
+	@Getter @Setter String date;
+	@Getter @Setter String description;
+	@Getter @Setter String location;
+	@Getter @Setter String status;
+	
 }
--- src/main/java/com/vetapp/application/status/StatusController.java
+++ src/main/java/com/vetapp/application/status/StatusController.java
@@ -1,5 +1,22 @@
 package com.vetapp.application.status;
 
-public class StatusController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StatusController {
+	
+	@Autowired
+	StatusRepository statusRepository;
+	
+	@GetMapping("/api/animals/{animalid}/status")
+	public ResponseEntity<List<Status>> getAnimalStatus(@PathVariable String animalid) {
+		return new ResponseEntity<>(statusRepository.findByAnimalidContaining(animalid), HttpStatus.ACCEPTED);
+	}
+	
+	
 }
