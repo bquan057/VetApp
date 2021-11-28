d6293c444b005c4bdc7a38f62cbd8ba69e5dae06
Rohinesh Ram <rohinesh@ualberta.ca>
2021-11-28 16:18:52 -0700

    Completed sidebar

---
+++ FrontEnd/vet-application/src/components/SideBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const SideBar = () => {
+
+    return(
+        <div className="column is-one-fifth has-background-primary">
+            <div className="hero is-centered is-fullheight">
+                <div className="container">
+                    <div className="buttons">
+                        <button  className="button has-text-weight-bold has-text-primary-dark is-rounded is-fullwidth">Manage Users</button>
+                        <button className= "button has-text-weight-bold has-text-primary-dark is-rounded is-fullwidth">Manage Animals</button>
+                    </div>
+                    <div className = "has-text-centered">
+                        <span class="icon-text">
+                            <div className="icon">
+                            <i className="far fa-user has-text-primary-dark"></i>
+                            </div>
+                            <a className= "has-text-weight-bold has-text-primary-dark" href="#" > Search Patient</a>
+                        </span>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default SideBar;
---
+++ FrontEnd/vet-application/src/views/SearchPage.js
@@ -0,0 +1,15 @@
+import React from "react"
+import SideBar from "../components/SideBar"
+import "bulma/css/bulma.css";
+
+
+const SearchPage= () => {
+
+    return(
+        <section class="hero has-background-success-light is-fullheight">
+            <SideBar/>
+        </section>
+    ) 
+};
+
+export default SearchPage;
